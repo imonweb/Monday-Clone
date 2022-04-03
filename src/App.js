0d20d43c13d313c0ec883993774c2f2c5a8015cf
Imon <contact@imonweb.co.uk>
2022-04-03 22:49:26 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
+import './App.css';
+import Nav from './components/Nav';
+import Dashboard from './pages/Dashboard';
+import Ticketpage from './pages/Ticketpage';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Nav/>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/' element={<Ticketpage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
